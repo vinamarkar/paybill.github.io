dcb4892d8ecadf1af4942033e95530ca6a0c6b08
vinamarkar <vinamarkar92td@gmail.com>
2018-07-23 02:17:11 +0530

    Paybill sample website

---
+++ footer.php
@@ -0,0 +1,278 @@
+<!-- footer -->
+	<div class="footer">
+		<div class="container">
+			<div class="row footerRow2">
+				<div class="col-md-2">
+					<h4 class="myAc"> My Account </h4>
+					<ul class="list-unstyled">
+						<li><a href="#" class="text-primary" title="login" data-toggle="modal" data-target="#login">My Account</i></a></li>
+						<li> Purchase History </li>
+						<li> Become a Partner </li>
+					</ul>
+				</div>
+				<div class="col-md-2">
+					<h4 class="myAc"> Services </h4>
+					<ul class="list-unstyled">
+						<li> <a href="account.html"> Send Top-up </a></li>
+						<li> <a href="full_coverage.html">Coverages</a></li>
+						<li> <a href="promotion.html">Promotions </a></li>
+					</ul>
+				</div>
+				<div class="col-md-2">
+					<h4 class="myAc"> Quick Links </h4>
+					<ul class="list-unstyled reck">
+						<li><a href="help.html"> Help </a> </li>
+						<li><a href="support.html" target="_blank"> Support </a> </li>
+						<li><a href="howItWorks.html" > How It Works</a> </li>
+					</ul>
+				</div>
+				<div class="col-md-2">
+					<h4 class="myAc"> Company </h4>
+					<ul class="list-unstyled">
+						<li> <a href="contactUs.html">Contact Us</a> </li>
+						<li> <a href="aboutUS.html"> About Us </a> </li>
+					</ul>
+				</div>
+				<div class="col-md-2">
+					<h4 class="myAc"> Languages </h4>
+					<ul class="list-unstyled">
+						<li> Us(EN) </li>
+						<li> Ind (Hindi)</li>
+						<li> Others </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="clear-fix">
+	</div>
+	<!--below footer -->
+<!--<div class="belowFooter">
+		<div class="container">
+			<div class="row footerRow2">
+				<div class="col-md-4">
+					<div class="col-md-4 col-xs-12" style="color:#0e4595">
+						verified by <br/>
+						<b>VISA</b>
+					</div>
+					<div class="col-md-4 col-xs-12" style="color:#cb0100">
+						<b>MasterCard</b> <br/>
+						SecureCode<br/>
+					</div>
+					<div class="col-md-4 col-xs-12" style="color:#002f86">
+						<b>PayPal</b>
+					</div>
+				</div>
+				<div class="col-md-8">
+					<span class="pull-right hidden-xs">
+						<p> <span style="color:#0e4595"><b>Sitelock</b></span>&nbsp;&nbsp;&nbsp; <span style="color:white"><b>Entrust</b></span>&nbsp;&nbsp;&nbsp; <span style="color:#cb0100"><b>Pci</b></span> </p>
+					</span>
+				</div>
+				<div class="col-md-12 col-pull-md-12 col-xs-12">
+					<span style="color:white">Privacy policy</span> &nbsp;&nbsp;&nbsp;
+					<span style="color:white">Terms and condition</span>
+					<span class="pull-right" class="color:white">Trustworthy, Secure and Protected </span>	
+				</div>
+			</div>
+		</div>
+	</div>-->
+	<div class="clear-fix">
+	</div>
+	<!-- bottom death footer -->
+	<div class="bottom-death-footer">
+		<div class="container">
+			<div class="row footerRow2">
+				<div class="col-md-12 col-xs-12">
+					<p class="bdmPara">Paybill a brand name of xxxx Pvt Ltd - registered in India</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="clear-fix"> 
+    </div>
+<!-- Modal for login -->
+		<div class="modal fade" id="login" role="dialog">
+			<div class="modal-dialog modal-dialog-001" style="">
+				<div class="modal-content">
+					<div class="modal-header" style="padding-bottom:5px;">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4> Login </h4>
+					</div>
+					<div style="padding:10px;"> </div>
+					<div class="row">
+						<div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1" >
+							<form>
+								<div class="form-group input-group">
+									<span class="input-group-addon" id="basic-addon1"><i class="fa fa-envelope"></i></span>
+									<input type="email" class="form-control" id="email" placeholder="enter email"/>
+								</div>
+								<div class="form-group input-group">
+									<span class="input-group-addon" id="basic-addon1"><i class="fa fa-key"></i></span>
+									<input type="password" class="form-control" id="email" placeholder="password"/>
+								</div> 
+								<div class="form-group">
+									<a href="account.html" type="submit" class="btn btn-success form-control">LOGIN</a> <br/>
+								</div>
+								<div class="form-group">
+									<ul class="list-unstyled text-center">
+										<li style="border-bottom:1px solid #e5e5e5"><a href="#" data-toggle="modal" data-dismiss="modal" data-target="#forgotPassword"> Forgot Password </a> </li>
+										<li><a href="#" data-toggle="modal" data-dismiss="modal" data-target="#signup"> Signup here </a> </li>
+									</ul>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--modal login over-->
+
+		<!-- Modal for Register -->
+		<div class="modal fade" id="signup" role="dialog">
+			<div class="modal-dialog modal-dialog-001" style="">
+				<div class="modal-content">
+					<div class="modal-header" style="padding-bottom:5px;">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4> Register </h4>
+					</div>
+					<div style="padding:10px;"> </div>
+					<div class="row">
+						<div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1" >
+							<form>
+								<div class="form-group input-group">
+									<span class="input-group-addon" id="basic-addon1"><i class="fa fa-envelope"></i></span>
+									<input type="email" class="form-control" id="email" placeholder="enter email" />
+								</div>
+								<div class="form-group input-group">
+									<span class="input-group-addon" id="basic-addon1"><i class="fa fa-key"></i></span>
+									<input type="password" class="form-control" id="email" placeholder="password" />
+								</div> 
+								<div class="form-group">
+									<ul class="list-unstyled text-center">
+										<li>Already have an account<a href="#" data-toggle="modal" data-dismiss="modal" data-target="#login"> Sign In </a> here </li>
+									</ul>
+								</div>
+									<div class="form-group">
+									<button type="submit" class="btn btn-success form-control">REGISTER</button> <br/>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+		<!--modal Register over-->
+		
+		<!-- Modal for Forgot password -->
+		<div class="modal fade" id="forgotPassword" role="dialog">
+			<div class="modal-dialog modal-dialog-001" style="">
+				<div class="modal-content">
+					<div class="modal-header" style="padding-bottom:5px;">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4> Forgot Password </h4>
+					</div>
+					<div style="padding:10px;"> </div>
+					<div class="row">
+						<div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1" >
+							<form>
+								<div class="form-group input-group">
+									<span class="input-group-addon" id="basic-addon1"><i class="fa fa-envelope"></i></span>
+									<input type="email" class="form-control" id="email" placeholder="enter email" />
+								</div>
+								<div class="form-group">
+									<button type="submit" class="btn btn-success form-control">RESET PASSWORD</button> <br/>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+		<!--modal forgot password over-->
+		<!--modal for Browse my Plans-->
+		
+		<div class="modal fade" id="browse" role="dialog">
+			<div class="modal-dialog modal-lg" style="">
+				<div class="modal-content">
+					<div class="modal-header" style="padding-bottom:5px;">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4> Browse My Plans </h4>
+					</div>
+					<div style="padding:10px;"> </div>
+					<div class="row">
+						<form>
+							<div class="col-md-7 col-md-offset-1">
+								<div class="form-group input-group">
+									<span class="input-group-addon" id="basic-addon1"><i class="fa fa-mobile fa-lg"></i></span>
+									<input type="email" class="form-control" id="email" placeholder="+91-97XXX-XX231" />
+								</div>
+							</div>
+							<div class="col-md-3 form-group">
+								<button type="submit" class="btn btn-success form-control">CHANGE PLANS</button>
+							</div>
+							<div class="col-md-12">
+							<hr class="mar6"/>
+							</div>
+						</form>	
+						<div class="col-md-10 col-md-offset-1">
+							<div class="planBorder">
+								<div class="col-md-4">
+									<div class="planCur">
+										<p style="margin-top:25px;"><span class="clrB siz19">THB</span></p>
+										<p>10</p>
+									</div>
+									<div class="planCur1 text-center">
+										<p class="mtop4 mbotm4">INR 19</p>
+									</div>
+								</div>
+								<div class="col-md-7">
+									<div class="planCurHead">
+										<h3> 100 Topups Last month </h3><br/>
+										<h3> Topup in 5 seconds </h3>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-md-offset-1">
+							<a href="#" class=""><div class="planBorder">
+								<div class="planCur">
+									<p style="margin-top:25px;"><span class="clrB siz19">THB</span></p>
+									<p>10</p>
+								</div>
+								<div class="planCur1 text-center">
+									<p class="mtop4 mbotm4">INR 19</p>
+								</div>
+							</div> </a>	
+						</div>
+						<div class="col-md-3">
+							<div class="planBorder">
+								<div class="planCur">
+									<p style="margin-top:25px;"><span class="clrB siz19">THB</span></p>
+									<p>50</p>
+								</div>
+								<div class="planCur1 text-center">
+									<p class="mtop4 mbotm4">INR 95</p>
+								</div>
+							</div>	
+						</div>
+						<div class="col-md-3">
+							<div class="planBorder">
+								<div class="planCur">
+									<p style="margin-top:25px;"><span class="clrB siz19">THB</span></p>
+									<p>100</p>
+								</div>
+								<div class="planCur1 text-center">
+									<p class="mtop4 mbotm4">INR 190</p>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Global online recharge - send instant mobile recharge, bill payment, data bundle, digital TV recharge, utility bill payment, bus card reload &amp;">
+    <title>Home Page</title>
+    <meta name="keywords" content="Send Instant Online Mobile Recharge | online prepaid data bundle recharge | online Digital TV Recharge | online Electricity Bill payment | online WIFI recharge | online Grocery voucher">
+    <meta name="author" content="Paybillus - Online Recharge">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+<!-- top header -->
+    <div class="top-header">
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-2 col-lg-2">
+                        <div class="navbar-header">
+                            <div class="navbar-brand">
+                                <a href="#"><span style="color:#337ab7">Pay</span><span style="color:#6FC041">bill</span></a>
+							</div>
+							<div class="barbtn1"> 
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainHeader" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+							</div>
+							<div class="pull-right hidden-lg hidden-sm hidden-md" style="margin:15px 10px 0px 0px;">
+								<ul class="list-unstyled list-inline">
+									<li><a href="#" class="text-primary" title="login" data-toggle="modal" data-target="#login"><i class="fa fa-lock fa-md"></i></a></li>
+									<li><a href="#" class="text-success" title="signup" data-toggle="modal" data-target="#signup"><i class="fa fa-user fa-md"></i></a></li>
+								</ul>
+							</div>
+                        </div>
+					</div>
+                    <div class="col-xs-12 col-sm-6 col-md-10 col-lg-10">
+					    <div class="collapse navbar-collapse" id="mainHeader">
+                            <ul class="nav navbar-nav left_navigation">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="promotion.html">Promotion</a></li>
+                                <li><a href="full_coverage.html">Full coverage</a></li>
+                                <li><a href="howItWorks.html">How It Works</a></li>
+                                <li><a href="support.html" target="_blank"> Support </a> </li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right hidden-xs">
+                                <li><a class="dropdown-toggle" role="button" data-toggle="dropdown">USD<span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">INR</a></li>
+                                        <li><a href="#">Baht</a></li>
+                                        <li><a href="#">EURO</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a class="dropdown-toggle" role="button" data-toggle="dropdown">US en<span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Chinese</a></li>
+                                        <li><a href="#">Thai</a></li>
+                                        <li><a href="#">German</a></li>
+                                    </ul>
+                                </li>
+                               <li>
+									<a href="#" data-toggle="modal" data-target="#login"><i class="fa fa-lock fa-md"></i> Login </a>
+                                </li>
+                                <li>
+                                    <a href="#" data-toggle="modal" data-target="#signup"><i class="fa fa-user fa-md"></i> Signup </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+	<div class="clear-fix">
+	</div>
+	<!-- sub header -->
+	<div class="Sub-header">
+        <nav class="navbar navbar-default1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-12 col-lg-12">
+                        <div class="barbtn2">
+								<button type="button" class="navbar-toggle collapsed bar2" data-toggle="collapse" data-target="#subHeader" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar bar2"></span>
+									<span class="icon-bar bar2"></span>
+									<span class="icon-bar bar2"></span>
+								</button>
+						</div>
+						<div class="collapse navbar-collapse" id="subHeader">
+                            <ul class="nav navbar-nav bar1">
+                                <li><a href="mobile.html"><b> Mobile </b></a></li>
+                                <li><a href="dth.html"><b> Digital TV </b></a></li>
+                                <li><a href="broadband.html"><b> Broadband </b></a></li>
+                                <li><a href="landline.html"><b> Landline </b></a></li>
+                                <li><a href="wifi.html"><b> Wifi </b></a></li>
+								<li><a href="eb.html"><b> Electricity </b></a></li>
+                            </ul>
+						</div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div> 
+	<div class="clear-fix">
+	</div>
+
+	
